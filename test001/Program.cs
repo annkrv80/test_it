11dcf4ddb2572d4640ec45ae5881bafbe94e96b1
Anna <annkrv80@yandex.ru>
2022-09-20 18:34:53 +0700

    Ввод массива пользователем с клавиатуры

---
+++ test001/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите число эллементов массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+FillingArray(array);
+
+
+
+
+void FillingArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");
+        string letter=Console.ReadLine()??"";
+
+        if(letter!="")
+        {
+            array[i] = letter;
+        }
+        else
+        {
+            Console.WriteLine ("Ошибка! Введите данные!");
+            break;
+        }
+    }
+}
